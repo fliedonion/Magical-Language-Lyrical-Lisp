d89bc2ffea6359c82ec4381fb41b8b65571765c2
flied onion <->
2012-06-09 23:38:41 +0900

    change: Map F2-key to enter lisp interpreter when textbtnwait.

---
+++ createNscript_dat/createNscript_dat_cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace createNscript_dat_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var wfs = new FileStream("nscript.dat", FileMode.Create ,FileAccess.Write))
+            {
+                using (var bw = new BinaryWriter(wfs,Encoding.ASCII))
+                {
+                    var sjis = Encoding.GetEncoding(932);
+
+                    foreach (var f in Directory.GetFiles(".", "*.txt"))
+                    {
+                        using (var sr = new StreamReader(f, sjis))
+                        {
+                            string line;
+                            while( (line = sr.ReadLine()) != null){
+                                foreach (var b in sjis.GetBytes(line))
+                                {
+                                    bw.Write((byte)(b ^ 0x84));
+                                }
+                                bw.Write((byte)(0x0A ^ 0x84));
+                            }
+                        }
+                    }
+                }
+            }
+            
+
+        }
+
+
+    }
+}
---
+++ createNscript_dat_cs_readme.txt
@@ -0,0 +1,9 @@
+createNscript_dat_cs
+author: flied onion
+
+カレントディレクトリの*.txtを読み込みnscript.datを作成する。
+作成作業が面倒だったので適当に作った。
+Windows / c#.NET 4.0
+
+c:\path\to\sources\> createNscript_dat_cs.exe
+
